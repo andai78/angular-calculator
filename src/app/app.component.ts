539812245407ca5c9be45a1382cd98123ec35f19
andai78 <tyrel92@hotmail.com>
2020-03-12 15:47:38 +0100

    creation calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'calculator';
+  currentNumber: string = '0';
+  firstNumber: number = null;
+  operator: string = null;
+  isSecondNumber: boolean = false;
+
+  public keyPress(num: string){
+    if(this.isSecondNumber)
+    {
+      this.currentNumber = num;
+      this.isSecondNumber = false;
+    }
+    else {
+      this.currentNumber === '0'? this.currentNumber = num: this.currentNumber += num;
+    }
+  }
+
+  public getDecimal() {
+    if(!this.currentNumber.includes('.')){
+        this.currentNumber += '.'; 
+    }
+  }
+
+  private calculate(operator , secondNumber){
+    switch (operator){
+      case '+':
+      return this.firstNumber += secondNumber; 
+      case '-': 
+      return this.firstNumber -= secondNumber; 
+      case '*': 
+      return this.firstNumber *= secondNumber; 
+      case '/': 
+      return this.firstNumber /= secondNumber; 
+      case '=':
+      return secondNumber;
+    }
+  }
+
+  public getOperator(op: string){
+
+    if(this.firstNumber === null){
+      this.firstNumber = Number(this.currentNumber);
+
+    } else if(this.operator) {
+      const result = this.calculate(this.operator , Number(this.currentNumber))
+      this.currentNumber = String(result);
+      this.firstNumber = result;
+    }
+    this.operator = op;
+    this.isSecondNumber = true;
+
+  }
+
+  public allClear() {
+    this.currentNumber = '0';
+    this.firstNumber = null;
+    this.operator = null;
+    this.isSecondNumber = false;
+  }
+
 }
